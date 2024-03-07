8693928d98a43dba2e3f9057cddb303b793c5171
Balint <balint@kovari.cc>
2024-03-07 19:19:33 +0100

    try with secureboot

--- common/secureboot.nix
+++ common/secureboot.nix
@@ -8,12 +8,16 @@ let
   };
 in {
   imports = [
-    # "${lanzaboote}/nixosModules/lanzaboot/"
     "${lanzaboote}/nix/modules/lanzaboote.nix"
   ];
+  #
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.lanzaboote = {
     enable = true;
     pkiBundle = "/etc/secureboot";
   };
+  #
+  environment.systemPackages = with pkgs; [
+    sbctl  # for troubleshooting Secure Boot
+  ];
 }
